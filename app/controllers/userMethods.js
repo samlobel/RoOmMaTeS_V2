dd32fef91ab5b9ab8f97c55d0756497f3c15e9ba
Sam Lobel <samlobel@yahoo.com>
2015-04-28 13:28:48 -0400

    GET REQUESTS HAVE QUERY STRINGS, NOT BODIES

--- app/controllers/userMethods.js
+++ app/controllers/userMethods.js
@@ -9,11 +9,13 @@ module.exports = {
       get users whose name starts with a certain prefix.
     */
     console.log('getUsers called');
+    console.log("body next ");
+    console.log(req.body)
     //console.log(req);
     var modelType = 'User';
     var field = 'username';
     var minLen = 2;
-    var prefix = req.body.prefix;
+    var prefix = req.query.prefix;
     var returnFields = ['username', '_id'];
 
     //this should send the response.
