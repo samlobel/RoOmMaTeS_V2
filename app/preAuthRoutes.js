07c35ece8bee9d02d1d4c406e45b93e54baa3b48
Derick Olson <olsonderick@gmail.com>
2015-04-29 08:02:19 -0400

    add venmo routes

--- app/preAuthRoutes.js
+++ app/preAuthRoutes.js
@@ -1,9 +1,10 @@
-var models = require('./models/index.js')
-var _ = require('underscore')
+var models = require('./models/index.js');
+var _ = require('underscore');
 
 
 module.exports = function(app, passport){
 
+/* Login Routes */
   app.post('/login', function(req,res){
     console.log("trying to log in");
     passport.authenticate('local-login', function(err, user, info){
@@ -47,14 +48,56 @@ module.exports = function(app, passport){
   });
 
 
+/* Venmo Routes */
+  var data;
+  
   app.get('/venmo-auth', function(req,res){
-    console.log("Registering venmo");
+    console.log('Venmo tried to authorize');
+
+    // Venmo unauthorized
     var error = req.query.error;
     if(error){
       console.log(error);
       return res.redirect('RoommatesApp://');
-
     }
+
+    console.log('Venmo use authorized');
+    console.log(req.query);
+
+    // Send Auth data back to venmo
+    data = {
+      'client_id' : '2583',
+      'client_secret' : 'PcY324UY8Fdhs3RCsUR5c4Nthd2Hy6Mx',
+      'code' :  req.query.code
+    }
+    var url = 'https://api.venmo.com/v1/oauth/access_token';
+
+    console.log("Throw redirect");
+    res.redirect('/catch-venmo');
+
+  });
+
+
+  app.get('/catch-venmo', function (req, res) {
+    console.log('Caught redirect');
+
+    // Post auth data to venmo
+    var request = require('request');
+    console.log(request);
+    request.post(
+      'https://api.venmo.com/v1/oauth/access_token',
+      data,
+      function (err, res, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('Got venmo credentials');
+            console.log(body);
+
+            // Save credentials
+        }
+      });
+
+    // Return to app
+    res.redirect('RoommatesApp://');
   });
 
 }
