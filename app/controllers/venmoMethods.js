31be6492a919d82b2592918931e04a24c18c2414
Sam Lobel <samlobel@yahoo.com>
2015-04-29 00:40:06 -0400

    commented out one of the sockets

--- app/controllers/venmoMethods.js
+++ app/controllers/venmoMethods.js
@@ -3,10 +3,14 @@ var _ = require('underscore')
 var helperFunctions = require('./helperFunctions')
 
 
+
+// GET /venmo-auth?error=User+denied+your+application+access+to+his+or+her+protected+resources. 500 1.028 ms - 28
+// that was what venmo sent when I hit "deny"
+
 module.exports = {
   hasBeenValidated: function hasBeenValidated(){
     var userID = req.user._id;
-    
+
   }
 
 }
